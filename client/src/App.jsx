360f7f6fcbb4adb704285de5f1aa8d53449a039b
Cristina <65986306+ceaguilar93@users.noreply.github.com>
2024-03-13 19:06:35 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//Here is where my great project starts
 function App() {
   const [count, setCount] = useState(0)
 
