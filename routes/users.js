52365aeafe8693f638385153ebb8e306db950b13
Cristina <65986306+ceaguilar93@users.noreply.github.com>
2024-03-22 16:21:42 +0100

    App.jsx and users.js changed
    App.jsc => console.log added to proof response
    users.js => endpoints created buy not linked front end with backedn

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// POST endpoint to store user information
+router.post("/user", async (req, res, next) => {
+  try {
+    const username = req.body; // Exctract the user information from the request body
+    const newUser = await User.create({username}); // Creates a new user in the database
+    res.status(201).json({message:"User created succesfully", user: newUser});
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({message:"Internal server error"});
+  } 
+  });
+
+// POST endpoint to store color analysis data
+  router.post('/color-analysis', async (req, res, next) => {
+    try {
+      const { userId, skinTone, hairColor, eyeColor, analysisResult } = req.body; // Extract color analysis data from the request body
+      const newColorAnalysis = await ColorAnalysis.create({ // Store color analysis data in the database
+        user_id: userId, 
+        skin_tone: skinTone, 
+        hair_color: hairColor, 
+        eye_color: eyeColor, 
+        analysis_result: analysisResult 
+      });
+    res.status(201).json({ message: 'Color analysis data stored successfully', colorAnalysis: newColorAnalysis }); // Success message storing the color analysis in the db
+    } catch (error) { // Handle errors
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
 
 module.exports = router;
